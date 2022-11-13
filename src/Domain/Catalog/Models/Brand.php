0fb40313401f127582b61cd3ab3bd2555cdee078
Ivan <ivan-it@r777.ru>
2022-11-13 18:09:22 +0300

    stage 4-4

--- src/Domain/Catalog/Models/Brand.php
+++ src/Domain/Catalog/Models/Brand.php
@@ -4,6 +4,8 @@ namespace Domain\Catalog\Models;
 
 use App\Models\Product;
 use Database\Factories\BrandFactory;
+use Domain\Catalog\QueryBuilders\BrandQueryBuilder;
+use Domain\Catalog\QueryBuilders\CategoryQueryBuilder;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -11,6 +13,9 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Support\Traits\Models\HasSlug;
 use Support\Traits\Models\HasThumbnail;
 
+/**
+ * @method static Brand|BrandQueryBuilder query()
+ */
 class Brand extends Model
 {
     use HasFactory;
@@ -32,11 +37,9 @@ class Brand extends Model
         return 'brands';
     }
 
-    public function scopeHomePage(Builder $query)
+    public function newEloquentBuilder($query): BrandQueryBuilder
     {
-        $query->where('on_home_page', true)
-            ->orderBy('sorting')
-            ->limit(6);
+        return new BrandQueryBuilder($query);
     }
 
     public function products(): HasMany
