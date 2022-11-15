5f9838906e63b00da518ed6f8ef319e0849f1dc7
Ivan <ivan-it@r777.ru>
2022-11-15 10:19:26 +0300

    Stage 5-front

---
+++ app/Http/Controllers/CatalogController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Domain\Catalog\Models\Brand;
+use Domain\Catalog\Models\Category;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+
+class CatalogController extends Controller
+{
+    public function page(?Category $category): Factory|View|Application
+    {
+        $brands = Brand::query()
+            ->select(['id', 'title'])
+            ->has('products')
+            ->get();
+
+        $categories = Category::query()
+            ->select(['id', 'title', 'slug'])
+            ->has('products')
+            ->get();
+
+        $products = Product::query()
+            ->select(['id', 'title', 'slug', 'price', 'thumbnail'])
+            ->paginate(6);
+
+        return view('catalog.catalog-page', compact('brands', 'categories', 'products', 'category'));
+    }
+}
---
+++ app/Routing/CatalogRegistrar.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Routing;
+
+use App\Contracts\RouteRegistrar;
+use App\Http\Controllers\CatalogController;
+use Illuminate\Contracts\Routing\Registrar;
+use Illuminate\Support\Facades\Route;
+
+class CatalogRegistrar implements RouteRegistrar
+{
+    public function map(Registrar $registrar)
+    {
+        Route::middleware('web')->group(function () {
+
+            Route::controller(CatalogController::class)->group(function () {
+                /**@see CatalogController::page()*/
+                Route::get('/catalog/{category:slug?}', 'page')
+                    ->name('catalog');
+            });
+        });
+    }
+}
